724d902736f4bb3cfc7fa0f32ad79692033c0a6c
Arkadiusz Matlak <piccolo@poczta.fm>
2023-07-05 23:44:43 +0200

    Firebase - config

--- src/pages/lib/firebase.js
+++ src/pages/lib/firebase.js
@@ -1,17 +1,16 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration
+
 const firebaseConfig = {
-    apiKey: process.env.APP_FIREBASE_KEY,
-    authDomain: process.env.APP_FIREBASE_AUTHDOMAIN,
-    projectId: process.env.APP_FIREBASE_PROJECTID,
-    storageBucket: process.env.APP_FIREBASE_STORAGEBUCKET,
-    messagingSenderId: process.env.APP_FIREBASE_MESSAGINGSENDERID,
-    appId: process.env.APP_FIREBASE_APPID
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTHDOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECTID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGEBUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGINGSENDERID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APPID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENTID
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
