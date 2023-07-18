f4a201dba4891f27dbec540e8ef2db1975c8f7c7
Arkadiusz Matlak <piccolo@poczta.fm>
2023-07-18 21:18:38 +0200

    refactoring - 230718

--- src/components/ModelsAcordion.jsx
+++ src/components/ModelsAcordion.jsx
@@ -51,7 +51,7 @@ export default function ModelsAccordion({models}) {
                                                   href={`/seryjne/${value.marka}/${value.rodzina}/${value.generacja}/${value.typ_nadwozia}/${value.liczba_drzwi}/${value.model}`}
                                             >
                                                 <Typography>
-                                                    {value.model} ({value.rok_uruch}-{value.rok_zakoncz})
+                                                    {value.model} ({value.rok_uruch}-{value.rok_zakoncz === 0 ? "..." : value.rok_zakoncz})
                                                 </Typography>
                                             </Link>
                                         </AccordionDetails>
