217186c3729e43100a5264900d8f8592a46cbd57
Arkadiusz Matlak <piccolo@poczta.fm>
2023-08-18 22:39:27 +0200

    modification of source path  for article pictures

--- src/components/ArticleContainer.jsx
+++ src/components/ArticleContainer.jsx
@@ -20,7 +20,7 @@ function ArticleContainer(props){
                       <Box key={index} sx={{display: "flex", mb: 2, py: 2, width: { xs: "100%", sm: "80%" }, borderTop: 2, borderBottom: 2, borderColor: 'red', marginInline: "auto"}}>
                         <Box sx={{marginInline: "auto"}}>
                             <img
-                                src={`/images/articles/${paragraph}`} alt={`Zdjęcie do artykułu`} style={{maxWidth: "500px", width: "100%"}}
+                                src={`http://server090121.nazwa.pl/images/articles/${paragraph}`} alt={`Zdjęcie do artykułu`} style={{maxWidth: "500px", width: "100%"}}
                             />
                         </Box>
                         </Box>
