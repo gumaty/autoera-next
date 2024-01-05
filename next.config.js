9f23bd2c8fb6fc2bbcc25867443b44fb0940f190
Arkadiusz Matlak <piccolo@poczta.fm>
2024-01-05 19:54:02 +0100

    redirect

--- next.config.js
+++ next.config.js
@@ -1,6 +1,38 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+	reactStrictMode: true,
+};
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+module.exports = {
+	async redirects() {
+		return [
+			{
+				source: '/prod.php:path*',
+				destination: '/seryjne',
+				permanent: false,
+			},
+			{
+				source: '/stud.php:path*',
+				destination: '/studialne',
+				permanent: false,
+			},
+			{
+				source: '/encyk.php:path*',
+				destination: '/encyklopedia',
+				permanent: false,
+			},
+			{
+				source: '/about.php3:path*',
+				destination: '/about',
+				permanent: false,
+			},
+			{
+				source: '/kontakt.php3:path*',
+				destination: '/kontakt',
+				permanent: false,
+			},
+		];
+	},
+};
